c7a3e489c8408ca90c88eeaa844b1ad2d0194e73
𖢵 || 𝗦ʏɴαxx ||𖢵 <159287148+Sagexdd@users.noreply.github.com>
2024-07-01 15:44:21 +0530

    Create config.py
    Signed-off-by: 𖢵 || 𝗦ʏɴαxx ||𖢵     <159287148+Sagexdd@users.noreply.github.com>

---
+++ config.py
@@ -0,0 +1,11 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "21827814"
+API_HASH = "af097d21fa5049b0ed0d614ed3b885cd"
+
+BOT_TOKEN = "7292639285:AAEGAyV1mKZ_CNB-14tqIRefxSM0559HJGg"
+#OWNER_ID = "7376728791"
+OWNER_ID = "7376728791"
